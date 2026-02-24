4411cea3327ccc46f2f3869e8f5337ac402f1586
tumatidurga <tumatidurga25@gmail.com>
2026-02-24 12:09:24 +0530

    Add solution for Move All Zeros to End

---
+++ Easy/move-zeros-to-end.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def moveZeros(nums: List[int]) -> List[int]:
+    # Your code here
+    pass
+
+# Read input
+nums = list(map(int, input().split()))
+result = moveZeros(nums)
+print(' '.join(map(str, result)))
