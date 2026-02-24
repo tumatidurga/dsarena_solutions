cb58ec6090d1c59ca8c3a4bbf07a3e41f77f4df1
tumatidurga <tumatidurga25@gmail.com>
2026-02-24 12:14:41 +0530

    Add solution for Remove Duplicates While Maintaining Order

---
+++ python/remove-duplicates-maintain-order.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def removeDuplicates(nums: List[int]) -> List[int]:
+    # Your code here
+    pass
+
+# Read input
+nums = list(map(int, input().split()))
+result = removeDuplicates(nums)
+print(' '.join(map(str, result)))
